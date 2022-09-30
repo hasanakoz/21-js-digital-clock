591bd89f589347cdcbef2bdf064aed4047ccb500
hasan <hasanakoz1986@gmail.com>
2022-09-30 23:27:43 +0300

    style properties changed

--- app.js
+++ app.js
@@ -18,33 +18,36 @@ setInterval(() => {
   second < 10
     ? (secondArea.innerHTML = `:0${second}`)
     : (secondArea.innerHTML = `:${second}`);
-}, 1000);
-
-setInterval(() => {
   minute = new Date().getMinutes();
   minuteArea.innerHTML = `:${minute}`;
-}, 1000);
-
-setInterval(() => {
   hour = new Date().getHours();
   hour < 10
     ? (hourArea.innerHTML = `0${hour}`)
     : (hourArea.innerHTML = `${hour}`);
-}, 1000);
-
-setInterval(() => {
   day = new Date().getDate();
   day < 10 ? (dayArea.innerHTML = `0${day}`) : (dayArea.innerHTML = `${day}`);
-}, 1000);
-
-setInterval(() => {
   month = new Date().getMonth();
   month < 10
-    ? (monthArea.innerHTML = `:0${month}`)
-    : (monthArea.innerHTML = `:${month}`);
-}, 1000);
-
-setInterval(() => {
+    ? (monthArea.innerHTML = `:0${month + 1}`)
+    : (monthArea.innerHTML = `:${month + 1}`);
   year = new Date().getFullYear();
   yearArea.innerHTML = `:${year}`;
 }, 1000);
+
+// setInterval(() => {}, 1000);
+
+// setInterval(() => {
+
+// }, 1000);
+
+// setInterval(() => {
+
+// }, 1000);
+
+// setInterval(() => {
+
+// }, 1000);
+
+// setInterval(() => {
+
+// }, 1000);
